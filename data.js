991a4b555e7b331c036fe621b13b3e48f58c6d88
Sepehr <95886974+SepehrDev256@users.noreply.github.com>
2022-05-24 01:29:22 +0430

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'Songs/song1.mp3',
+        artist: 'artist 1',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+    {
+        name: 'song 2',
+        path: 'Songs/song2.mp3',
+        artist: 'artist 2',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+    {
+        name: 'song 3',
+        path: 'Songs/song3.mp3',
+        artist: 'artist 3',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+    {
+        name: 'song 4',
+        path: 'Songs/song4.mp3',
+        artist: 'artist 4',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+    {
+        name: 'song 5',
+        path: 'Songs/song5.mp3',
+        artist: 'artist 5',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+    {
+        name: 'song 6',
+        path: 'Songs/song6.mp3',
+        artist: 'artist 6',
+        cover: 'Image/Huawei-Nova-8-640x396-1.png'
+    },
+]
